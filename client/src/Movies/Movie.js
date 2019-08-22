409133583eb39ad774baec7da9f96b78bc0a75b9
antilou86 <antilou86@gmail.com>
2019-08-22 16:32:28 -0400

    WIP: edit form completed. moving on to delete.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import { Link } from 'react-router-dom';
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -25,7 +27,9 @@ export default class Movie extends React.Component {
       .then(res => this.setState({ movie: res.data }))
       .catch(err => console.log(err.response));
   };
-
+  editMovie = () => {
+    props.history.push('/');
+  }
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
     addToSavedList(this.state.movie);
@@ -42,6 +46,11 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <Link to={`"/update-movie/${this.state.movie.id}`}>
+          <div className="save-button">
+            Edit
+          </div>
+        </Link>
       </div>
     );
   }
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const UpdateMovie = props => {
@@ -14,22 +14,22 @@ const UpdateMovie = props => {
         const id = props.match.params.id;
         const movieInArr = props.movies.find(mov => `${mov.id}` === id);
         if (movieInArr) setMovie(movieInArr);
-      }, [props.movies, props.match.params.id]);
-    
+    }, [props.movies, props.match.params.id]);
+
 
     const handleSubmit = () => {
         e.preventDefault();
-    axios
-      .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
-      .then(res => {
-        console.log(res);
-        setMovie(initialState);
-        props.updateItems(res.data);
-        props.history.push('/');
-      })
-      .catch(err => console.log(err.response));
-     }
-    
+        axios
+            .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+            .then(res => {
+                console.log(res);
+                setMovie(initialState);
+                props.updateItems(res.data);
+                props.history.push('/');
+            })
+            .catch(err => console.log(err.response));
+    }
+
     const changeHandler = (ev) => {
         ev.persist();
         let value = ev.target.value;
@@ -42,45 +42,50 @@ const UpdateMovie = props => {
         });
     };
 
-return (
-    <>
-        <h2>Update Movie</h2>
-        <form onSubmit={handleSubmit}>
-            <label>Title:</label><br />
-            <input
-                type="text"
-                name="title"
-                onChange={changeHandler}
-                placeholder="Title..."
-                value={movie.title}
-            /><br />
-            <label>Director:</label><br />
-            <input
-                type="text"
-                name="director"
-                onChange={changeHandler}
-                placeholder="Director..."
-                value={movie.director}
-            /><br />
-            <label>Metascore:</label><br />
-            <input
-                type="number"
-                name="metascore"
-                onChange={changeHandler}
-                placeholder="Metascore..."
-                value={movie.metascore}
-            /><br />
-            <label>Stars:</label><br />
-            <input
-                type="text"
-                name="stars"
-                onChange={changeHandler}
-                placeholder="Stars..."
-                value={movie.stars} /><br />
-            <button>Update!</button>
-        </form>
-    </>
-)
+    const starChangeHandler = (ind, e) => {
+        const newStars = [...movie.stars];
+        newStars[ind] = e.target.value;
+        setMovie({...movie, stars: newStars})
+    }
+    return (
+        <>
+            <h2>Update Movie</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Title:</label><br />
+                <input
+                    type="text"
+                    name="title"
+                    onChange={changeHandler}
+                    placeholder="Title..."
+                    value={movie.title}
+                /><br />
+                <label>Director:</label><br />
+                <input
+                    type="text"
+                    name="director"
+                    onChange={changeHandler}
+                    placeholder="Director..."
+                    value={movie.director}
+                /><br />
+                <label>Metascore:</label><br />
+                <input
+                    type="number"
+                    name="metascore"
+                    onChange={changeHandler}
+                    placeholder="Metascore..."
+                    value={movie.metascore}
+                /><br />
+                <label>Stars:</label><br />
+                <input
+                    type="text"
+                    name="stars"
+                    onChange={e => starChangeHandler(ind, e)}
+                    placeholder="Stars..."
+                    value={movie.stars[ind]} /><br />
+                <button>Update!</button>
+            </form>
+        </>
+    )
 }
 
 export default UpdateMovie
